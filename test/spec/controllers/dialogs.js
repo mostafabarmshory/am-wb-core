e9be263df43487881910e9e355ced8a5de0d7784
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2016-10-23 00:09:03 +0330

    Product is generated

--- test/spec/controllers/dialogs.js
+++ test/spec/controllers/dialogs.js
@@ -1,28 +1,28 @@
 'use strict';
 
-describe('Controller: DialogsCtrl', function () {
+describe('Controller: DialogsCtrl', function() {
 
-  // load the controller's module
-  beforeEach(module('ngMaterialWeburger'));
+    // load the controller's module
+    beforeEach(module('ngMaterialWeburger'));
 
-  var DialogsCtrl;
-  var scope;
-  var ngModel;
-  var mdDialog;
+    var DialogsCtrl;
+    var scope;
+    var ngModel;
+    var mdDialog;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    DialogsCtrl = $controller('DialogsCtrl', {
-      $scope: scope,
-      $mdDialog: mdDialog,
-      mdeModel: ngModel,
-      style: {}
-      // place here mocked dependencies
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope) {
+	scope = $rootScope.$new();
+	DialogsCtrl = $controller('DialogsCtrl', {
+	    $scope : scope,
+	    $mdDialog : mdDialog,
+	    wbModel : ngModel,
+	    style : {}
+	// place here mocked dependencies
+	});
+    }));
+
+    it('should attach a list of awesomeThings to the scope', function() {
+	expect(DialogsCtrl).not.toBe(null);
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-//    expect(DialogsCtrl.awesomeThings.length).toBe(3);
-  });
 });
