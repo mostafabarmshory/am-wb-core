656e58422051f9641ef6a4919335b90e3829a375
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2016-12-30 14:28:04 +0330

    Add license file.

--- src/scripts/controllers/wb-textsetting.js
+++ src/scripts/controllers/wb-textsetting.js
@@ -1,38 +1,55 @@
+/* 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 weburger
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+'use strict';
+angular.module('ngMaterialWeburger')
+
 /**
  * Created by mgh on 8/10/2016.
  */
-
-/* jslint todo: true */
-/* jslint xxx: true */
-/* jshint -W100 */
-
-'use strict';
-angular
-	.module('ngMaterialWeburger')
-
-	.controller(
-		'WbTextSettingsCtrl',
-		function($scope) {
-		    var scope = $scope;
-		    scope.tinymceOptions = {
-			/*
-			 * onChange: function(e) { // put logic here for
-			 * keypress and cut/paste changes },
-			 */
-			/*
-			 * selector: 'textarea', inline: false, plugins :
-			 * 'advlist autolink link image lists charmap print
-			 * preview', skin: 'lightgray', theme : 'modern',
-			 * font_formats: 'Arial=arial,helvetica,sans-serif;'
-			 */
-			selector : 'textarea',
-			height : 500,
-			theme : 'modern',
-			plugins : [
-				'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-				'searchreplace wordcount visualblocks visualchars code fullscreen',
-				'insertdatetime media nonbreaking save table contextmenu directionality',
-				'emoticons template paste textcolor colorpicker textpattern imagetools' ],
+.controller(
+	'WbTextSettingsCtrl',
+	function($scope) {
+	    var scope = $scope;
+	    scope.tinymceOptions = {
+		    /*
+		     * onChange: function(e) { // put logic here for
+		     * keypress and cut/paste changes },
+		     */
+		    /*
+		     * selector: 'textarea', inline: false, plugins :
+		     * 'advlist autolink link image lists charmap print
+		     * preview', skin: 'lightgray', theme : 'modern',
+		     * font_formats: 'Arial=arial,helvetica,sans-serif;'
+		     */
+		    selector : 'textarea',
+		    height : 500,
+		    theme : 'modern',
+		    plugins : [
+			'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+			'searchreplace wordcount visualblocks visualchars code fullscreen',
+			'insertdatetime media nonbreaking save table contextmenu directionality',
+			'emoticons template paste textcolor colorpicker textpattern imagetools' ],
 			toolbar1 : 'fontselect fontsizeselect | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
 			toolbar2 : 'print preview media | forecolor backcolor emoticons',
 			image_advtab : true,
@@ -44,8 +61,8 @@ angular
 			    content : 'Test 2'
 			} ],
 			content_css : [
-				'//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-				'//www.tinymce.com/css/codepen.min.css' ]
-		    };
+			    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+			    '//www.tinymce.com/css/codepen.min.css' ]
+	    };
 
-		});
+	});
--- src/scripts/controllers/wb-widgetselect.js
+++ src/scripts/controllers/wb-widgetselect.js
@@ -1,6 +1,26 @@
-/* jslint todo: true */
-/* jslint xxx: true */
-/* jshint -W100 */
+/* 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 weburger
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 'use strict';
 
 angular.module('ngMaterialWeburger')
@@ -14,53 +34,55 @@ angular.module('ngMaterialWeburger')
  * در این کنترل امکاناتی فراهم شده که کاربر بتواند از میان ویجت‌های موجودی یکی
  * را انتخاب کند.
  */
-.controller('WbWidgetSelectCtrl', function($scope, $widget, PaginatorParameter) {
-	var scope = $scope;
-	var paginatorParameter = new PaginatorParameter();
+.controller('WbWidgetSelectCtrl',
+	function($scope, $widget, PaginatorParameter) {
+	    var scope = $scope;
+	    var paginatorParameter = new PaginatorParameter();
 
-	/**
-	 * ویجت‌های موجود را لود می‌کند
-	 * 
-	 * تمام ویجت‌های لود شده در متغیری به نام widgets توی اسکپ بار می‌شود.
-	 * 
-	 */
-	function loadWidgets() {
+	    /**
+	     * ویجت‌های موجود را لود می‌کند
+	     * 
+	     * تمام ویجت‌های لود شده در متغیری به نام widgets توی اسکپ بار
+	     * می‌شود.
+	     * 
+	     */
+	    function loadWidgets() {
 		$widget.widgets(paginatorParameter).then(function(widgets) {
-			scope.widgets = widgets;
-			selectWidget(widgets.items[0]);
+		    scope.widgets = widgets;
+		    selectWidget(widgets.items[0]);
 		});
-	}
+	    }
 
-	/**
-	 * ویجت پیش فرض را تعیین می‌کند
-	 * 
-	 * با انتخاب یک ویجت به عنوان ویجت پیش فرض می‌توان نمایش خاصی از آن را در
-	 * سیستم ایجاد کرد.
-	 * 
-	 * @memberof WbWidgetSelectCtrl
-	 * @param {Widget}
-	 *            widget ویجت پیش فرض را تعیین می‌کند
-	 * @returns
-	 */
-	function selectWidget(widget) {
+	    /**
+	     * ویجت پیش فرض را تعیین می‌کند
+	     * 
+	     * با انتخاب یک ویجت به عنوان ویجت پیش فرض می‌توان نمایش خاصی از آن
+	     * را در سیستم ایجاد کرد.
+	     * 
+	     * @memberof WbWidgetSelectCtrl
+	     * @param {Widget}
+	     *                widget ویجت پیش فرض را تعیین می‌کند
+	     * @returns
+	     */
+	    function selectWidget(widget) {
 		scope.cwidget = widget;
 		// TODO: bind the widget
-	}
+	    }
 
-	/**
-	 * ویجت را به عنوان ویجت انتخاب شده تعیین می‌کندs 
-	 * 
-	 * @memberof WbWidgetSelectCtrl
-	 * @param widget
-	 * @returns
-	 */
-	function answerWidget(widget) {
+	    /**
+	     * ویجت را به عنوان ویجت انتخاب شده تعیین می‌کندs
+	     * 
+	     * @memberof WbWidgetSelectCtrl
+	     * @param widget
+	     * @returns
+	     */
+	    function answerWidget(widget) {
 		var element = angular.copy(widget.data);
 		$scope.answer(element);
-	}
+	    }
 
-	// تعیین خصوصیت‌های اسکوپ
-	scope.selectWidget = selectWidget;
-	scope.answerWidget = answerWidget;
-	loadWidgets();
-});
+	    // تعیین خصوصیت‌های اسکوپ
+	    scope.selectWidget = selectWidget;
+	    scope.answerWidget = answerWidget;
+	    loadWidgets();
+	});
