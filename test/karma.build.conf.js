d3d1faa21df2806c937a57020b0d5a09a32a7d7c
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-09-29 15:53:16 +0330

    New build of the group

--- test/karma.build.conf.js
+++ test/karma.build.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
 			'bower_components/handsontable/dist/handsontable.js',
 			'bower_components/ngHandsontable/dist/ngHandsontable.js',
 			'bower_components/tinycolor/tinycolor.js',
-			'bower_components/md-color-picker/dist/mdColorPicker.min.js',
+			'bower_components/md-color-picker/dist/md-color-picker.js',
 			'bower_components/angular-drag-and-drop-lists/angular-drag-and-drop-lists.js',
 			'bower_components/jspanel4x/dist/jspanel.js',
 			'bower_components/lodash/lodash.js',
