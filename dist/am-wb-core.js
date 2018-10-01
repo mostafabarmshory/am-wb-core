8de0c73029474037a5052af5bc4faf3c1d3e0f15
masood <masood@DESKTOP-GV3RBT4>
2018-10-02 01:48:40 +0330

    https://taskulu.com/a/project/5b2501d456ad66341e08ec40/tasks/5baf7fa156ad66096c464dbe

--- dist/am-wb-core.js
+++ dist/am-wb-core.js
@@ -2291,6 +2291,162 @@ angular.module('am-wb-core')
 	};
 });
 
+///* 
+// * The MIT License (MIT)
+// * 
+// * Copyright (c) 2016 weburger
+// * 
+// * Permission is hereby granted, free of charge, to any person obtaining a copy
+// * of this software and associated documentation files (the "Software"), to deal
+// * in the Software without restriction, including without limitation the rights
+// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// * copies of the Software, and to permit persons to whom the Software is
+// * furnished to do so, subject to the following conditions:
+// * 
+// * The above copyright notice and this permission notice shall be included in all
+// * copies or substantial portions of the Software.
+// * 
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// * SOFTWARE.
+// */
+//'use strict';
+//
+//angular.module('am-wb-core')
+//
+//        /**
+//         * @ngdoc Directives
+//         * @name wbUiSettingBorderRadius
+//         * @author maso<mostafa.barmshory@dpq.co.ir>
+//         * @author hadi<mohammad.hadi.mansouri@dpq.co.ir>
+//         * @author Masood<masoodzarei64@gmail.com>
+//         * @description Set border radius (css based)
+//         * 
+//         * @see https://www.w3schools.com/cssref/css_units.asp
+//         */
+//        .directive('wbUiSettingBorderRadius', function () {
+//            return {
+//                templateUrl: 'views/directives/wb-ui-setting-border-radius.html',
+//                restrict: 'E',
+//                replace: true,
+//                scope: {
+//                    title: '@?',
+//                    value: '=?',
+//                    icon: '@?',
+//                    description: '@?'
+//                },
+//                /*
+//                 * @ngInject
+//                 */
+//                controller: function ($scope /*$resource*/) {
+//                    /*
+//                     * splice the different radious number from value.
+//                     * the format of value is: 'ddpx ddpx ddpx ddpx' which d is digit, dd < 100 and px is pixel.
+//                     * for example value = '02px 10px 08px 20px'. 
+//                     */
+////                    if ($scope.value) {
+////                        var topLeft = $scope.value.slice(0, 2);
+////                        var topRight = $scope.value.slice(5, 7);
+////                        var bottomRight = $scope.value.slice(10, 12);
+////                        var bottomLeft = $scope.value.slice(15, 17);
+////                    }
+////
+////                    //check if all values are equal
+////                    if (topLeft === topRight && bottomLeft === bottomRight && topLeft === bottomLeft) {
+////                        $scope.allEqual = true;
+////                        $scope.allCorner = topLeft;//or any one.
+////                    } else {
+////                        $scope.allEqual = false;
+////                        $scope.topLeft = topLeft;
+////                        $scope.topRight = topRight;
+////                        $scope.bottomLeft = bottomLeft;
+////                        $scope.bottomRight = bottomRight;
+////                    }
+////
+//                    $scope.$watch('allCorner', function (val) {
+//                        $scope.value.all = val;
+////                        var str = '';
+////                        var newVal = '';
+////                        if(val === 'undefined'){
+////                            return;
+////                        }
+////                        if (val < 10) {
+////                            val = '0' + val;
+////                        }
+////                        str = val + 'px';
+////                        newVal = str + ' ' + str + ' ' + str + ' ' + str;
+////                        $scope.value = newVal;
+//                    });
+////
+////                    /*
+////                     * watch all corner radius
+////                     */
+//                    $scope.$watch('topLeft', function (val) {
+//                        $scope.value.topLeft = val;
+////                        if(val === 'undefined'){
+////                            return;
+////                        }
+////                        if (val < 10) {
+////                            val = '0' + val;
+////                        }
+////                        topLeft = val;
+////                        setRadiusValue();
+//                    });
+//
+//                    $scope.$watch('topRight', function (val) {
+//                        $scope.value.topLeft = val;
+////                        if(val === 'undefined'){
+////                            return;
+////                        }
+////                        if (val < 10) {
+////                            val = '0' + val;
+////                        }
+////                        topRight = val;
+////                        setRadiusValue();
+//                    });
+//
+//                    $scope.$watch('bottomLeft', function (val) {
+//                        $scope.value.topLeft = val;
+////                        if(val === 'undefined'){
+////                            return;
+////                        }
+////                        if (val < 10) {
+////                            val = '0' + val;
+////                        }
+////                        bottomLeft = val;
+////                        setRadiusValue();
+//                    });
+//
+//                    $scope.$watch('bottomRight', function (val) {
+//                        $scope.value.topLeft = val;
+////                        if(val === 'undefined'){
+////                            return;
+////                        }
+////                        if (val < 10) {
+////                            val = '0' + val;
+////                        }
+////                        bottomRight = val;
+////                        setRadiusValue();
+//                    });
+////
+////                    /*
+////                     * set $scope.value when the value of each corner radius is changed
+////                     */
+////                    var setRadiusValue = function () {
+////                        $scope.value =
+////                                topLeft + 'px' + ' '
+////                                + topRight + 'px' + ' '
+////                                + bottomRight + 'px' + ' '
+////                                + bottomLeft + 'px';
+////                    };
+//                },
+//                controllerAs: 'ctrl'
+//            };
+//        });
 /* 
  * The MIT License (MIT)
  * 
@@ -5540,8 +5696,13 @@ angular.module('am-wb-core').run(['$templateCache', function($templateCache) {
   );
 
 
+  $templateCache.put('views/directives/wb-ui-setting-border-radius.html',
+    ""
+  );
+
+
   $templateCache.put('views/directives/wb-ui-setting-border.html',
-    "<div layout=column> <p translate>Radius</p> <md-checkbox ng-model=allEqual ng-checked=allEqual> All equal </md-checkbox> <div ng-show=allEqual layout=row flex> <md-slider-container flex> <md-slider min=0 max=99 ng-model=allCorner> </md-slider> </md-slider-container> <md-input-container flex=20> <input type=number style=text-align:center ng-model=allCorner> </md-input-container> <p layout-align=\"end center\">px</p> </div> <div layout=column ng-show=!allEqual> <md-divider></md-divider> <div layout=row layout-align=\"space-between center\"> <p flex=25>Top left</p> <md-slider-container flex> <md-slider min=0 max=99 ng-model=topLeft> </md-slider> </md-slider-container> <md-input-container flex=15> <input type=number style=text-align:center ng-model=topLeft> </md-input-container> <p layout-align=\"end center\">px</p> </div> <div layout=row layout-align=\"space-between center\"> <p flex=25>Top right</p> <md-slider-container flex> <md-slider min=0 max=99 ng-model=topRight> </md-slider> </md-slider-container> <md-input-container flex=15> <input type=number style=text-align:center ng-model=topRight> </md-input-container> <p layout-align=\"end center\">px</p> </div> <div layout=row layout-align=\"space-between center\"> <p flex=25>Bottom right</p> <md-slider-container flex> <md-slider min=0 max=99 ng-model=bottomRight> </md-slider> </md-slider-container> <md-input-container flex=15> <input type=number style=text-align:center ng-model=bottomRight> </md-input-container> <p layout-align=\"end center\">px</p> </div> <div layout=row layout-align=\"space-between center\"> <p flex=25>Bottom left</p> <md-slider-container flex> <md-slider min=0 max=99 ng-model=bottomLeft> </md-slider> </md-slider-container> <md-input-container flex=15> <input type=number style=text-align:center ng-model=bottomLeft> </md-input-container> <p layout-align=\"end center\">px</p> </div> </div> <div class=hint ng-if=description translate=\"\">{{description}} </div> </div>"
+    "<div layout=column> <p translate>Radius</p> <md-checkbox ng-model=allEqual ng-checked=allEqual> All equal </md-checkbox> <div ng-show=allEqual layout=row flex> <md-slider-container flex> <md-slider min=0 max=99 ng-model=allCorner> </md-slider> </md-slider-container> <md-input-container flex=20> <input type=number style=text-align:center ng-model=allCorner> </md-input-container> <p layout-align=\"end center\">px</p> </div> <div layout=column ng-show=!allEqual> <md-divider></md-divider> <md-list-item> <wb-icon>corner_top_left</wb-icon> <md-slider min=0 max=99 ng-model=topLeft flex> </md-slider> <md-input-container flex=15> <input type=number style=text-align:center ng-model=topLeft> </md-input-container> </md-list-item> <md-list-item> <wb-icon>corner_top_right</wb-icon> <md-slider min=0 max=99 ng-model=topRight flex> </md-slider> <md-input-container flex=15> <input type=number style=text-align:center ng-model=topRight> </md-input-container> </md-list-item> <md-list-item> <wb-icon>corner_bottom_right</wb-icon> <md-slider min=0 max=99 ng-model=bottomRight flex> </md-slider> <md-input-container flex=15> <input type=number style=text-align:center ng-model=bottomRight> </md-input-container> </md-list-item> <md-list-item> <wb-icon>corner_bottom_left</wb-icon> <md-slider min=0 max=99 ng-model=bottomLeft flex> </md-slider> <md-input-container flex=15> <input type=number style=text-align:center ng-model=bottomLeft> </md-input-container> </md-list-item>   <div class=hint ng-if=description translate=\"\">{{description}} </div> </div> </div>"
   );
 
 
