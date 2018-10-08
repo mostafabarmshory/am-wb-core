11fc44513a6bc21f484d7fac62e0e1c2781ac224
masood <masood@DESKTOP-GV3RBT4>
2018-10-08 18:37:57 +0330

    https://trello.com/c/TFOS47fK/26-%D8%AA%D9%86%D8%B8%DB%8C%D9%85%D9%87%D8%A7%DB%8C-%D9%85%D8%B1%D8%A8%D9%88%D8%B7-%D8%A8%D9%87-%D8%B9%D8%B1%D8%B6-%D9%88-%D8%A7%D8%B1%D8%AA%D9%81%D8%A7-%D8%A8%D8%B1-%D8%A7%D8%B3%D8%A7%D8%B3-%D9%85%D8%AF%D9%84-%D8%AC%D8%AF%DB%8C%D8%AF-%D8%A8%D8%A7%D8%B2%D8%A8%DB%8C%D9%86%DB%8C-%D8%B4%D9%88%D8%AF

--- src/scripts/runs/settings.js
+++ src/scripts/runs/settings.js
@@ -146,12 +146,12 @@ angular.module('am-wb-core')
                     });
 
                     function setAllWidth(dim, val) {
-                        
+
                         dim.top = val;
                         dim.right = val;
                         dim.bottom = val;
                         dim.left = val;
-                        
+
                     }
 
                     $scope.$watch('width', function (newWidth) {
@@ -171,14 +171,14 @@ angular.module('am-wb-core')
                      * watch 'wbModel' and apply the changes in setting panel
                      */
                     $scope.$watch('wbModel', function (model) {
-                        
+
                         //width is a string such as '10px 25% 2vh 4px'
                         var width = fillWidthFromString($scope.width, model.style.border.width || 'medium');
-                        
+
                         if (width) {
                             $scope.widthAll = width;
                         }
-                        
+
                     });
 
                     /*
@@ -187,21 +187,21 @@ angular.module('am-wb-core')
                      * https://www.w3schools.com/CSSref/pr_border-width.asp
                      */
                     function fillWidthFromString(dim, str) {
-                        
+
                         var dimAll;
                         var dimsArray = str.split(' ');
-                        
+
                         // 'medium' is selected
                         if (dimsArray.length === 1) {
                             dimAll = str;
                             return dimAll;
                         }
-                        
+
                         //Items are 4 and equal
                         else if (dimsArray.length === 4 && _.uniq(dimsArray).length === 1) {
                             dimAll = dimsArray[0];
-                        } 
-                        
+                        }
+
                         //Items are 4 and different
                         else if (dimsArray.length === 4 && _.uniq(dimsArray).length > 1) {
                             dim.top = dimsArray[0];
@@ -209,7 +209,7 @@ angular.module('am-wb-core')
                             dim.bottom = dimsArray[2];
                             dim.left = dimsArray[3];
                         }
-                        
+
                         //Items are 3
                         else if (dimsArray.length === 3) {
                             dim.top = dimsArray[0];
@@ -217,7 +217,7 @@ angular.module('am-wb-core')
                             dim.left = dimsArray[1];
                             dim.bottom = dimsArray[2];
                         }
-                        
+
                         //Items are 2
                         else if (dimsArray.length === 2) {
                             dim.top = dimsArray[0];
@@ -225,7 +225,7 @@ angular.module('am-wb-core')
                             dim.right = dimsArray[1];
                             dim.left = dimsArray[1];
                         }
-                        
+
                         //Items are 1
                         else if (dimsArray.length === 1) {
                             dim.top = dimsArray[0];
@@ -233,12 +233,12 @@ angular.module('am-wb-core')
                             dim.bottom = dimsArray[0];
                             dim.left = dimsArray[0];
                         }
-                        
+
                         //All items are undefined. In this case default value is 'medium'.
                         else if (!dimsArray.length) {
                             dimAll = 'medium';
                         }
-                        
+
                         return dimAll;
                     }
 
@@ -250,12 +250,12 @@ angular.module('am-wb-core')
                     });
 
                     function setAllRadius(dim, val) {
-                        
+
                         dim.topLeft = val;
                         dim.topRight = val;
                         dim.bottomRight = val;
                         dim.bottomLeft = val;
-                        
+
                     }
 
                     $scope.$watch('radius', function (newRadius) {
@@ -263,28 +263,28 @@ angular.module('am-wb-core')
                     }, true);
 
                     function createDimeRadiusStr(dim) {
-                        
+
                         var output =
                                 dim.topLeft + ' ' +
                                 dim.topRight + ' ' +
                                 dim.bottomRight + ' ' +
                                 dim.bottomLeft;
                         return output;
-                        
+
                     }
 
                     /*
                      * watch 'wbModel' and apply the changes in setting panel
                      */
                     $scope.$watch('wbModel', function (model) {
-                        
+
                         //radius is a string such as '10px 25% 2vh 4px'
                         var radius = fillRadiusFromString($scope.radius, model.style.border.radius || '0px');
-                        
+
                         if (radius) {
                             $scope.radiusAll = radius;
                         }
-                        
+
                     });
 
                     /*
@@ -293,21 +293,21 @@ angular.module('am-wb-core')
                      * https://www.w3schools.com/CSSref/css3_pr_border-radius.asp
                      */
                     function fillRadiusFromString(dim, str) {
-                        
+
                         var dimAll;
                         var dimsArray = str.split(' ');
-                        
+
                         // 0px is selected
                         if (dimsArray.length === 1) {
                             dimAll = str;
                             return dimAll;
                         }
-                        
+
                         //Items are 4 and equal
                         else if (dimsArray.length === 4 && _.uniq(dimsArray).length === 1) {
                             dimAll = dimsArray[0];
-                        } 
-                        
+                        }
+
                         //Items are 4 and different
                         else if (dimsArray.length === 4 && _.uniq(dimsArray).length > 1) {
                             dim.topLeft = dimsArray[0];
@@ -315,7 +315,7 @@ angular.module('am-wb-core')
                             dim.bottomRight = dimsArray[2];
                             dim.bottomLeft = dimsArray[3];
                         }
-                        
+
                         //Items are 3
                         else if (dimsArray.length === 3) {
                             dim.topLeft = dimsArray[0];
@@ -323,7 +323,7 @@ angular.module('am-wb-core')
                             dim.bottomLeft = dimsArray[1];
                             dim.bottomRight = dimsArray[2];
                         }
-                        
+
                         //Items are 2
                         else if (dimsArray.length === 2) {
                             dim.topLeft = dimsArray[0];
@@ -331,7 +331,7 @@ angular.module('am-wb-core')
                             dim.topRight = dimsArray[1];
                             dim.bottomLeft = dimsArray[1];
                         }
-                        
+
                         //Items are 1
                         else if (dimsArray.length === 1) {
                             dim.topLeft = dimsArray[0];
@@ -339,12 +339,12 @@ angular.module('am-wb-core')
                             dim.bottomRight = dimsArray[0];
                             dim.bottomLeft = dimsArray[0];
                         }
-                        
+
                         //All items are undefined. In this case default value is 'medium'.
                         else if (!dimsArray.length) {
                             dimAll = '0px';
                         }
-                        
+
                         return dimAll;
                     }
                 },
@@ -533,58 +533,58 @@ angular.module('am-wb-core')
                 controller: function ($scope) {
 
                     /*
-                     * All settings about margin
+                     * All settings about margin and padding
                      */
                     $scope.$watch('marginAll', function (val) {
                         setAllMargin($scope.margin, val || '0px');//default value of margin
                     });
-                    
+
                     $scope.$watch('paddingAll', function (val) {
                         setAllMargin($scope.padding, val || '0px');//default value of padding
                     });
 
                     function setAllMargin(dim, val) {
-                        
+
                         dim.top = val;
                         dim.right = val;
                         dim.bottom = val;
                         dim.left = val;
-                        
+
                     }
 
                     $scope.$watch('margin', function (newMargin) {//margin is object
                         $scope.wbModel.style.margin = createDimeStr(newMargin);
                     }, true);
-                    
+
                     $scope.$watch('padding', function (newPadding) {//padding is object
                         $scope.wbModel.style.padding = createDimeStr(newPadding);
                     }, true);
 
                     function createDimeStr(dim) {
-                        
+
                         var output =
                                 dim.top + ' ' +
                                 dim.right + ' ' +
                                 dim.bottom + ' ' +
                                 dim.left;
                         return output;
-                        
+
                     }
 
                     /*
                      * watch 'wbModel' and apply the changes in setting panel
                      */
                     $scope.$watch('wbModel', function (model) {
-                        
+
                         //margin is a string such as '10px 25% 2vh 4px'
                         var margin = fillFromString($scope.margin, model.style.margin || '0px');
-                        
+
                         if (margin) {
                             $scope.marginAll = margin;
                         }
-                        
+
                         var padding = fillFromString($scope.padding, model.style.padding || '0px');
-                        
+
                         if (padding) {
                             $scope.paddingAll = padding;
                         }
@@ -596,23 +596,23 @@ angular.module('am-wb-core')
                      * https://www.w3schools.com/cssref/pr_margin.asp
                      * https://www.w3schools.com/cssref/pr_padding.asp
                      */
-                    
+
                     function fillFromString(dim, str) {
-                        
+
                         var dimAll;
                         var dimsArray = str.split(' ');
-                        
+
                         // 0px is selected
-                        if(dimsArray.length === 1) {
+                        if (dimsArray.length === 1) {
                             dimAll = str;
                             return dimAll;
                         }
-                        
+
                         //All 4 items is equal
                         else if (dimsArray.length === 4 && _.uniq(dimsArray).length === 1) {
                             dimAll = dimsArray[0];
-                        } 
-                        
+                        }
+
                         //Items are 4 and different
                         else if (dimsArray.length === 4 && _.uniq(dimsArray).length > 1) {
                             dim.top = dimsArray[0];
@@ -620,7 +620,7 @@ angular.module('am-wb-core')
                             dim.bottom = dimsArray[2];
                             dim.left = dimsArray[3];
                         }
-                        
+
                         //Items are 3
                         else if (dimsArray.length === 3) {
                             dim.top = dimsArray[0];
@@ -628,7 +628,7 @@ angular.module('am-wb-core')
                             dim.left = dimsArray[1];
                             dim.bottom = dimsArray[2];
                         }
-                        
+
                         //Items are 2
                         else if (dimsArray.length === 2) {
                             dim.top = dimsArray[0];
@@ -636,7 +636,7 @@ angular.module('am-wb-core')
                             dim.right = dimsArray[1];
                             dim.left = dimsArray[1];
                         }
-                        
+
                         //Items are 1
                         else if (dimsArray.length === 1) {
                             dim.top = dimsArray[0];
@@ -644,12 +644,12 @@ angular.module('am-wb-core')
                             dim.bottom = dimsArray[0];
                             dim.left = dimsArray[0];
                         }
-                        
+
                         //All items are undefined. In this case default value is 0px.
                         else if (!dimsArray.length) {
                             dimAll = '0px';
                         }
-                        
+
                         return dimAll;
                     }
                 }
