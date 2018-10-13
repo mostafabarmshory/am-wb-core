48c748bd212e0c93f559dc19392d3abfd95bff70
masood <masood@DESKTOP-GV3RBT4>
2018-10-13 18:23:46 +0330

    https://taskulu.com/a/project/5b2501d456ad66341e08ec40/tasks/5bc1959a56ad6619674d8518
    The 'Text' setting is removed from setting panel. Instead, this property is added to the text widget.

--- src/scripts/runs/settings.js
+++ src/scripts/runs/settings.js
@@ -87,53 +87,6 @@ angular.module('am-wb-core')
             });
 
             $settings.newPage({
-                type: 'text',
-                label: 'Text',
-                icon: 'text_fields',
-                /*
-                 * @ngInject
-                 */
-                controller: function ($scope) {
-                    var scope = $scope;
-                    scope.tinymceOptions = {
-                        /*
-                         * onChange: function(e) { // put logic here for
-                         * keypress and cut/paste changes },
-                         */
-                        /*
-                         * selector: 'textarea', inline: false, plugins :
-                         * 'advlist autolink link image lists charmap print
-                         * preview', skin: 'lightgray', theme : 'modern',
-                         * font_formats: 'Arial=arial,helvetica,sans-serif;'
-                         */
-                        selector: 'textarea',
-                        height: 500,
-                        theme: 'modern',
-                        plugins: [
-                            'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-                            'searchreplace wordcount visualblocks visualchars code fullscreen',
-                            'insertdatetime media nonbreaking save table contextmenu directionality',
-                            'emoticons template paste textcolor colorpicker textpattern imagetools'],
-                        toolbar1: 'fontselect fontsizeselect | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-                        toolbar2: 'print preview media | forecolor backcolor emoticons',
-                        image_advtab: true,
-                        templates: [{
-                                title: 'Test template 1',
-                                content: 'Test 1'
-                            }, {
-                                title: 'Test template 2',
-                                content: 'Test 2'
-                            }],
-                        content_css: [
-                            '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-                            '//www.tinymce.com/css/codepen.min.css']
-                    };
-
-                },
-                templateUrl: 'views/settings/wb-text.html'
-            });
-
-            $settings.newPage({
                 type: 'description',
                 label: 'Description',
                 templateUrl: 'views/settings/wb-description.html'
