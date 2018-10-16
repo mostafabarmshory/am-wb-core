4fa235856b8cf47b0840e5fe0eb60998899a906a
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-10-16 19:53:03 +0330

    New build of the project

--- dist/am-wb-core.js
+++ dist/am-wb-core.js
@@ -469,7 +469,7 @@ angular.module('am-wb-core')
  * 
  * Note that, in smal screen devices, the colume layout apply as default.
  */
-.directive('wbLayout', function() {
+.directive('wbLayout', function($mdMedia) {
 
     /**
      * Adds layout config into the element
@@ -489,7 +489,7 @@ angular.module('am-wb-core')
         {
             flexLayout.display = 'flex';
             // row
-            if(layout.direction === 'row'){
+            if(layout.direction === 'row' && $mdMedia('gt-sm')){
                 flexLayout['flex-direction'] =  layout.direction_reverse? 'row-reverse' : 'row'; 
                 flexLayout['overflow-x'] = layout.wrap ? 'visible' : 'auto';
                 flexLayout['overflow-y'] = 'visible';
@@ -5935,6 +5935,265 @@ angular.module('am-wb-core')
 	service.compile = compile;
 });
 
+/* 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 weburger
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+'use strict';
+
+/**
+ * Binds a TinyMCE widget to <textarea> elements.
+ */
+angular.module('ui.tinymce', [])
+.value('uiTinymceConfig', {})
+.directive('uiTinymce', function($rootScope, $compile, $timeout, $window, $sce, uiTinymceConfig, uiTinymceService) {
+	uiTinymceConfig = uiTinymceConfig || {};
+
+	if (uiTinymceConfig.baseUrl) {
+		tinymce.baseURL = uiTinymceConfig.baseUrl;
+	}
+
+	return {
+		require: ['ngModel', '^?form'],
+		priority: 599,
+		link: function(scope, element, attrs, ctrls) {
+			if (!$window.tinymce) {
+				return;
+			}
+
+			var ngModel = ctrls[0],
+			form = ctrls[1] || null;
+
+			var expression, options = {
+					debounce: true
+			}, tinyInstance,
+			updateView = function(editor) {
+				var content = editor.getContent({format: options.format}).trim();
+				content = $sce.trustAsHtml(content);
+
+				ngModel.$setViewValue(content);
+				if (!$rootScope.$$phase) {
+					scope.$digest();
+				}
+			};
+
+			function toggleDisable(disabled) {
+				if (disabled) {
+					ensureInstance();
+
+					if (tinyInstance) {
+						tinyInstance.getBody().setAttribute('contenteditable', false);
+					}
+				} else {
+					ensureInstance();
+
+					if (tinyInstance && !tinyInstance.settings.readonly && tinyInstance.getDoc()) {
+						tinyInstance.getBody().setAttribute('contenteditable', true);
+					}
+				}
+			}
+
+			// fetch a unique ID from the service
+			var uniqueId = uiTinymceService.getUniqueId();
+			attrs.$set('id', uniqueId);
+
+			expression = {};
+
+			angular.extend(expression, scope.$eval(attrs.uiTinymce));
+
+			//Debounce update and save action
+			var debouncedUpdate = (function(debouncedUpdateDelay) {
+				var debouncedUpdateTimer;
+				return function(ed) {
+					$timeout.cancel(debouncedUpdateTimer);
+					debouncedUpdateTimer = $timeout(function() {
+						return (function(ed) {
+							if (ed.isDirty()) {
+								ed.save();
+								updateView(ed);
+							}
+						})(ed);
+					}, debouncedUpdateDelay);
+				};
+			})(400);
+
+			var setupOptions = {
+					// Update model when calling setContent
+					// (such as from the source editor popup)
+					setup: function(ed) {
+						ed.on('init', function() {
+							ngModel.$render();
+							ngModel.$setPristine();
+							ngModel.$setUntouched();
+							if (form) {
+								form.$setPristine();
+							}
+						});
+
+						// Update model when:
+						// - a button has been clicked [ExecCommand]
+						// - the editor content has been modified [change]
+						// - the node has changed [NodeChange]
+						// - an object has been resized (table, image) [ObjectResized]
+						ed.on('ExecCommand change NodeChange ObjectResized', function() {
+							if (!options.debounce) {
+								ed.save();
+								updateView(ed);
+								return;
+							}
+							debouncedUpdate(ed);
+						});
+
+						ed.on('blur', function() {
+							element[0].blur();
+							ngModel.$setTouched();
+							if (!$rootScope.$$phase) {
+								scope.$digest();
+							}
+						});
+
+						ed.on('remove', function() {
+							element.remove();
+						});
+
+						if (uiTinymceConfig.setup) {
+							uiTinymceConfig.setup(ed, {
+								updateView: updateView
+							});
+						}
+
+						if (expression.setup) {
+							expression.setup(ed, {
+								updateView: updateView
+							});
+						}
+					},
+					format: expression.format || 'html',
+					selector: '#' + attrs.id
+			};
+			// extend options with initial uiTinymceConfig and
+			// options from directive attribute value
+			angular.extend(options, uiTinymceConfig, expression, setupOptions);
+			// Wrapped in $timeout due to $tinymce:refresh implementation, requires
+			// element to be present in DOM before instantiating editor when
+			// re-rendering directive
+			$timeout(function() {
+				if (options.baseURL){
+					tinymce.baseURL = options.baseURL;
+				}
+				var maybeInitPromise = tinymce.init(options);
+				if(maybeInitPromise && typeof maybeInitPromise.then === 'function') {
+					maybeInitPromise.then(function() {
+						toggleDisable(scope.$eval(attrs.ngDisabled));
+					});
+				} else {
+					toggleDisable(scope.$eval(attrs.ngDisabled));
+				}
+			});
+
+			ngModel.$formatters.unshift(function(modelValue) {
+				return modelValue ? $sce.trustAsHtml(modelValue) : '';
+			});
+
+			ngModel.$parsers.unshift(function(viewValue) {
+				return viewValue ? $sce.getTrustedHtml(viewValue) : '';
+			});
+
+			ngModel.$render = function() {
+				ensureInstance();
+
+				var viewValue = ngModel.$viewValue ?
+						$sce.getTrustedHtml(ngModel.$viewValue) : '';
+
+						// instance.getDoc() check is a guard against null value
+						// when destruction & recreation of instances happen
+						if (tinyInstance &&
+								tinyInstance.getDoc()
+						) {
+							tinyInstance.setContent(viewValue);
+							// Triggering change event due to TinyMCE not firing event &
+							// becoming out of sync for change callbacks
+							tinyInstance.fire('change');
+						}
+			};
+
+			attrs.$observe('disabled', toggleDisable);
+
+			// This block is because of TinyMCE not playing well with removal and
+			// recreation of instances, requiring instances to have different
+			// selectors in order to render new instances properly
+			var unbindEventListener = scope.$on('$tinymce:refresh', function(e, id) {
+				var eid = attrs.id;
+				if (angular.isUndefined(id) || id === eid) {
+					var parentElement = element.parent();
+					var clonedElement = element.clone();
+					clonedElement.removeAttr('id');
+					clonedElement.removeAttr('style');
+					clonedElement.removeAttr('aria-hidden');
+					tinymce.execCommand('mceRemoveEditor', false, eid);
+					parentElement.append($compile(clonedElement)(scope));
+					unbindEventListener();
+				}
+			});
+
+			scope.$on('$destroy', function() {
+				ensureInstance();
+
+				if (tinyInstance) {
+					tinyInstance.remove();
+					tinyInstance = null;
+				}
+			});
+
+			function ensureInstance() {
+				if (!tinyInstance) {
+					tinyInstance = tinymce.get(attrs.id);
+				}
+			}
+		}
+	};
+})
+
+/**
+ * A service is used to create unique ID's, this prevents duplicate ID's if there are multiple editors on screen.
+ */
+.service('uiTinymceService', function() {
+	var UITinymceService = function() {
+		var ID_ATTR = 'ui-tinymce';
+		// uniqueId keeps track of the latest assigned ID
+		var uniqueId = 0;
+		// getUniqueId returns a unique ID
+		var getUniqueId = function() {
+			uniqueId ++;
+			return ID_ATTR + '-' + uniqueId;
+		};
+		// return the function as a public method of the service
+		return {
+			getUniqueId: getUniqueId
+		};
+	};
+	// return a new instance of the service
+	return new UITinymceService();
+});
 angular.module('am-wb-core').run(['$templateCache', function($templateCache) {
   'use strict';
 
@@ -6161,72 +6420,54 @@ angular.module('am-wb-core').run(['$templateCache', function($templateCache) {
 
   $templateCache.put('views/widgets/wb-html.html',
     " <div ng-if=!ctrl.isEditable() ng-bind-html=\"wbModel.text | wbunsafe\" class=\"wb-widget-fill wb-widget-text\"> </div> <div ng-if=ctrl.isEditable() ui-tinymce=\"{\n" +
-    "            selector : 'div.tinymce', \n" +
-    "            menubar: true,\n" +
+    "        selector : 'div.tinymce', \n" +
+    "        menubar: true,\n" +
     "\t    inline: true,\n" +
     "\t    theme: 'modern',\n" +
-    "            plugins : [\n" +
-    "                'advlist',\n" +
-    "                'autolink',\n" +
-    "                'autoresize',\n" +
-    "                'autosave',\n" +
-    "                'bbcode',\n" +
-    "                'charmap',\n" +
-    "                'code',\n" +
-    "                'codesample',\n" +
-    "                'colorpicker',\n" +
-    "                'contextmenu',\n" +
-    "                'directionality',\n" +
-    "                'emoticons',\n" +
-    "                'hr',\n" +
-    "                'image',\n" +
-    "                'imagetools',\n" +
-    "                'importcss',\n" +
-    "                'insertdatetime',\n" +
-    "                'legacyoutput',\n" +
-    "                'link',\n" +
-    "                'lists',\n" +
-    "                'media',\n" +
-    "                'nonbreaking',\n" +
-    "                'noneditable',\n" +
-    "                'paste',\n" +
-    "                'save',\n" +
-    "                'searchreplace',\n" +
-    "                'spellchecker',\n" +
-    "                'tabfocus',\n" +
-    "                'table',\n" +
-    "                'template',\n" +
-    "                'textcolor',\n" +
-    "                'textpattern',\n" +
-    "                'toc',\n" +
-    "                'visualblocks',\n" +
-    "                'wordcount'\n" +
-    "            ],\n" +
-    "            toolbar: [\n" +
-    "                'fullscreen | undo redo | bold italic underline | styleselect fontselect fontsizeselect | visualblocks',\n" +
-    "                'forecolor backcolor | ltr rtl | alignleft aligncenter alignjustify alignright alignfull | numlist bullist outdent indent'\n" +
-    "            ],\n" +
+    "        plugins : [\n" +
+    "            'advlist',\n" +
+    "            'autolink',\n" +
+    "            'autoresize',\n" +
+    "            'autosave',\n" +
+    "            'bbcode',\n" +
+    "            'charmap',\n" +
+    "            'code',\n" +
+    "            'codesample',\n" +
+    "            'colorpicker',\n" +
+    "            'contextmenu',\n" +
+    "            'directionality',\n" +
+    "            'emoticons',\n" +
+    "            'hr',\n" +
+    "            'image',\n" +
+    "            'imagetools',\n" +
+    "            'importcss',\n" +
+    "            'insertdatetime',\n" +
+    "            'legacyoutput',\n" +
+    "            'link',\n" +
+    "            'lists',\n" +
+    "            'media',\n" +
+    "            'nonbreaking',\n" +
+    "            'noneditable',\n" +
+    "            'paste',\n" +
+    "            'save',\n" +
+    "            'searchreplace',\n" +
+    "            'spellchecker',\n" +
+    "            'tabfocus',\n" +
+    "            'table',\n" +
+    "            'template',\n" +
+    "            'textcolor',\n" +
+    "            'textpattern',\n" +
+    "            'toc',\n" +
+    "            'visualblocks',\n" +
+    "            'wordcount'\n" +
+    "        ],\n" +
+    "        toolbar: [\n" +
+    "            'fullscreen | undo redo | bold italic underline | formatselect fontselect fontsizeselect | visualblocks',\n" +
+    "            'forecolor backcolor | ltr rtl | alignleft aligncenter alignjustify alignright alignfull | numlist bullist outdent indent'\n" +
+    "        ],\n" +
     "\t    powerpaste_word_import: 'clean',\n" +
     "\t    powerpaste_html_import: 'clean',\n" +
-    "            textpattern_patterns: [\n" +
-    "                {start: '*', end: '*', format: 'italic'},\n" +
-    "                {start: '**', end: '**', format: 'bold'},\n" +
-    "                {start: '#', format: 'h1'},\n" +
-    "                {start: '##', format: 'h2'},\n" +
-    "                {start: '###', format: 'h3'}\n" +
-    "            ],\n" +
-    "            format: 'raw',\n" +
-    "            \n" +
-    "            style_formats_merge: true,\n" +
-    "            style_formats: [\n" +
-    "                {title: 'Bold text', inline: 'b'},\n" +
-    "                {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},\n" +
-    "                {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},\n" +
-    "                {title: 'Example 1', inline: 'span', classes: 'example1'},\n" +
-    "                {title: 'Example 2', inline: 'span', classes: 'example2'},\n" +
-    "                {title: 'Table styles'},\n" +
-    "                {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}\n" +
-    "            ]\n" +
+    "        format: 'raw',\n" +
     "\t}\" ng-model=wbModel.text class=\"wb-widget-fill tinymce wb-widget-text\"> </div>"
   );
 
