f689dea06b8c5d216a36f4a97e25045b1766a6d5
masood <masood@DESKTOP-GV3RBT4>
2018-10-20 10:25:33 +0330

    Merge origin/develop into develop
    Conflicts:
    	app/scripts/configs/language.js
    	app/views/request.html
    	app/views/requests.html

--- src/scripts/services/util.js
+++ src/scripts/services/util.js
@@ -25,38 +25,38 @@
 /**
  * Utility class of WB
  */
-angular.module('am-wb-core').service('$wbUtil', function($q, $templateRequest, $sce) {
+angular.module('am-wb-core').service('$wbUtil', function ($q, $templateRequest, $sce) {
     'use strict';
     var service = this;
 
     function cleanMap(oldStyle, newStyle, map) {
-        for (var i = 0; i < map.length; i++) {
-            if (oldStyle[map[i][0]]) {
-                newStyle[map[i][1]] = oldStyle[map[i][0]];
-                delete oldStyle[map[i][0]];
-            }
-        }
+	for (var i = 0; i < map.length; i++) {
+	    if (oldStyle[map[i][0]]) {
+		newStyle[map[i][1]] = oldStyle[map[i][0]];
+		delete oldStyle[map[i][0]];
+	    }
+	}
     }
 
     function getTemplateOf(page)
     {
-        var template;
-        var templateUrl;
-        if (angular.isDefined(template = page.template)) {
-            if (angular.isFunction(template)) {
-                template = template(page.params);
-            }
-        } else if (angular
-                .isDefined(templateUrl = page.templateUrl)) {
-            if (angular.isFunction(templateUrl)) {
-                templateUrl = templateUrl(page.params);
-            }
-            if (angular.isDefined(templateUrl)) {
-                page.loadedTemplateUrl = $sce.valueOf(templateUrl);
-                template = $templateRequest(templateUrl);
-            }
-        }
-        return template;
+	var template;
+	var templateUrl;
+	if (angular.isDefined(template = page.template)) {
+	    if (angular.isFunction(template)) {
+		template = template(page.params);
+	    }
+	} else if (angular
+		.isDefined(templateUrl = page.templateUrl)) {
+	    if (angular.isFunction(templateUrl)) {
+		templateUrl = templateUrl(page.params);
+	    }
+	    if (angular.isDefined(templateUrl)) {
+		page.loadedTemplateUrl = $sce.valueOf(templateUrl);
+		template = $templateRequest(templateUrl);
+	    }
+	}
+	return template;
     }
 
     /**
@@ -70,209 +70,214 @@ angular.module('am-wb-core').service('$wbUtil', function($q, $templateRequest, $
      */
     function getTemplateFor(page)
     {
-        return $q.when(getTemplateOf(page));
+	return $q.when(getTemplateOf(page));
     }
 
     function cleanEvetns(model)
     {
-        // event
-        if (!model.event) {
-            model.event = {};
-        }
+	// event
+	if (!model.event) {
+	    model.event = {};
+	}
     }
 
     function cleanLayout(model)
     {
-        if (!model.style.layout) {
-            model.style.layout = {};
-        }
-        if (model.type === 'Group' || model.type === 'Page') {
-            // convert
-            var newStyle = model.style.layout;
-            var oldStyle = model.style;
+	if (!model.style.layout) {
+	    model.style.layout = {};
+	}
+	if (model.type === 'Group' || model.type === 'Page') {
+	    // convert
+	    var newStyle = model.style.layout;
+	    var oldStyle = model.style;
 
-            if (oldStyle.flexDirection) {
-                if (oldStyle.flexDirection === 'wb-flex-row') {
-                    newStyle.direction = 'row';
-                } else {
-                    newStyle.direction = 'column';
-                }
-                delete oldStyle.flexDirection;
-            }
-            if (!newStyle.direction) {
-                newStyle.direction = 'column';
-            }
+	    if (oldStyle.flexDirection) {
+		if (oldStyle.flexDirection === 'wb-flex-row') {
+		    newStyle.direction = 'row';
+		} else {
+		    newStyle.direction = 'column';
+		}
+		delete oldStyle.flexDirection;
+	    }
+	    if (!newStyle.direction) {
+		newStyle.direction = 'column';
+	    }
 
-            switch (oldStyle.flexAlignItem) {
-            case 'wb-flex-align-items-center':
-                newStyle.align = 'center';
-                break;
-            case 'wb-flex-align-items-end':
-                newStyle.align = 'end';
-                break;
-            case 'wb-flex-align-items-start':
-                newStyle.align = 'start';
-                break;
-            case 'wb-flex-align-items-stretch':
-                newStyle.align = 'stretch';
-                break;
-            default:
-                newStyle.align = 'stretch';
-            }
-            delete oldStyle.flexAlignItem;
+	    switch (oldStyle.flexAlignItem) {
+		case 'wb-flex-align-items-center':
+		    newStyle.align = 'center';
+		    break;
+		case 'wb-flex-align-items-end':
+		    newStyle.align = 'end';
+		    break;
+		case 'wb-flex-align-items-start':
+		    newStyle.align = 'start';
+		    break;
+		case 'wb-flex-align-items-stretch':
+		    newStyle.align = 'stretch';
+		    break;
+		default:
+		    newStyle.align = 'stretch';
+	    }
+	    delete oldStyle.flexAlignItem;
 
-            switch (oldStyle.justifyContent) {
-            case 'wb-flex-justify-content-center':
-                newStyle.justify = 'center';
-                break;
-            case 'wb-flex-justify-content-end':
-                newStyle.justify = 'end';
-                break;
-            case 'wb-flex-justify-content-start':
-                newStyle.justify = 'start';
-                break;
-            case 'wb-flex-justify-content-space-between':
-                newStyle.justify = 'space-between';
-                break;
-            case 'wb-flex-justify-content-space-around':
-                newStyle.justify = 'space-around';
-                break;
-            default:
-                newStyle.justify = 'center';
-            }
-            delete oldStyle.justifyContent;
-        }
+	    switch (oldStyle.justifyContent) {
+		case 'wb-flex-justify-content-center':
+		    newStyle.justify = 'center';
+		    break;
+		case 'wb-flex-justify-content-end':
+		    newStyle.justify = 'end';
+		    break;
+		case 'wb-flex-justify-content-start':
+		    newStyle.justify = 'start';
+		    break;
+		case 'wb-flex-justify-content-space-between':
+		    newStyle.justify = 'space-between';
+		    break;
+		case 'wb-flex-justify-content-space-around':
+		    newStyle.justify = 'space-around';
+		    break;
+		default:
+		    newStyle.justify = 'center';
+	    }
+	    delete oldStyle.justifyContent;
+	}
     }
 
     function cleanSize(model)
     {
-        if (!model.style.size) {
-            model.style.size = {};
-        }
-        var newStyle = model.style.size;
-        var oldStyle = model.style;
-        var map = [ [ 'width', 'width' ],
-            [ 'height', 'height' ] ];
-        cleanMap(oldStyle, newStyle, map);
+	if (!model.style.size) {
+	    model.style.size = {};
+	}
+	var newStyle = model.style.size;
+	var oldStyle = model.style;
+	var map = [['width', 'width'],
+	    ['height', 'height']];
+	cleanMap(oldStyle, newStyle, map);
     }
 
     function cleanBackground(model)
     {
-        if (!model.style.background) {
-            model.style.background = {};
-        }
-        var newStyle = model.style.background;
-        var oldStyle = model.style;
-        var map = [ [ 'backgroundImage', 'image' ],
-            [ 'backgroundColor', 'color' ],
-            [ 'backgroundSize', 'size' ],
-            [ 'backgroundRepeat', 'repeat' ],
-            [ 'backgroundPosition', 'position' ] ];
-        cleanMap(oldStyle, newStyle, map);
+	if (!model.style.background) {
+	    model.style.background = {};
+	}
+	var newStyle = model.style.background;
+	var oldStyle = model.style;
+	var map = [['backgroundImage', 'image'],
+	    ['backgroundColor', 'color'],
+	    ['backgroundSize', 'size'],
+	    ['backgroundRepeat', 'repeat'],
+	    ['backgroundPosition', 'position']];
+	cleanMap(oldStyle, newStyle, map);
     }
 
     function cleanBorder(model)
     {
-        if (!model.style.border) {
-            model.style.border = {};
-        }
-        var oldStyle = model.style;
-        var newStyle = model.style.border;
+	if (!model.style.border) {
+	    model.style.border = {};
+	}
+	var oldStyle = model.style;
+	var newStyle = model.style.border;
 
-        if (oldStyle.borderRadius) {
-            if (oldStyle.borderRadius.uniform) {
-                newStyle.radius = oldStyle.borderRadius.all + 'px';
-            }
-            // TODO: maso, 2018: support other models
-        }
-        // delete old values
-        delete model.style.borderColor;
-        delete model.style.borderRadius;
-        delete model.style.borderStyleColorWidth;
-        delete model.style.borderStyle;
-        delete model.style.borderWidth;
+	if (oldStyle.borderRadius) {
+	    if (oldStyle.borderRadius.uniform) {
+		newStyle.radius = oldStyle.borderRadius.all + 'px';
+	    }
+	    // TODO: maso, 2018: support other models
+	}
+	// delete old values
+	delete model.style.borderColor;
+	delete model.style.borderRadius;
+	delete model.style.borderStyleColorWidth;
+	delete model.style.borderStyle;
+	delete model.style.borderWidth;
     }
 
     function cleanSpace(model)
     {
-        // Margin and padding
-        if (model.style.padding && angular.isObject(model.style.padding)) {
-            var padding = '';
-            if (model.style.padding.isUniform) {
-                padding = model.style.padding.uniform;
-            } else {
-                padding = model.style.padding.top || '0px' + ' ' + 
-                model.style.padding.right || '0px' + ' ' + 
-                model.style.padding.bottom || '0px' + ' ' + 
-                model.style.padding.left || '0px' + ' ';
-            }
-            model.style.padding = padding;
-        }
+	// Margin and padding
+	if (model.style.padding && angular.isObject(model.style.padding)) {
+	    var padding = '';
+	    if (model.style.padding.isUniform) {
+		padding = model.style.padding.uniform;
+	    } else {
+		padding = model.style.padding.top || '0px' + ' ' +
+			model.style.padding.right || '0px' + ' ' +
+			model.style.padding.bottom || '0px' + ' ' +
+			model.style.padding.left || '0px' + ' ';
+	    }
+	    model.style.padding = padding;
+	}
 
-        if (model.style.margin && angular.isObject(model.style.margin)) {
-            var margin = '';
-            if (model.style.margin.isUniform) {
-                margin = model.style.margin.uniform;
-            } else {
-                margin = model.style.margin.top || '0px' + ' ' + 
-                model.style.margin.right || '0px' + ' ' + 
-                model.style.margin.bottom || '0px' + ' ' + 
-                model.style.margin.left || '0px' + ' ';
-            }
-            model.style.margin = margin;
-        }
+	if (model.style.margin && angular.isObject(model.style.margin)) {
+	    var margin = '';
+	    if (model.style.margin.isUniform) {
+		margin = model.style.margin.uniform;
+	    } else {
+		margin = model.style.margin.top || '0px' + ' ' +
+			model.style.margin.right || '0px' + ' ' +
+			model.style.margin.bottom || '0px' + ' ' +
+			model.style.margin.left || '0px' + ' ';
+	    }
+	    model.style.margin = margin;
+	}
 
     }
 
     function cleanAlign(model)
     {
-        if (!model.style.align) {
-            model.style.align = {};
-        }
+	if (!model.style.align) {
+	    model.style.align = {};
+	}
     }
 
     function cleanStyle(model)
     {
-        if (!model.style) {
-            model.style = {};
-        }
-        cleanLayout(model);
-        cleanSize(model);
-        cleanBackground(model);
-        cleanBorder(model);
-        cleanSpace(model);
-        cleanAlign(model);
+	if (!model.style) {
+	    model.style = {};
+	}
+	cleanLayout(model);
+	cleanSize(model);
+	cleanBackground(model);
+	cleanBorder(model);
+	cleanSpace(model);
+	cleanAlign(model);
     }
 
     function cleanInternal(model)
     {
-        cleanEvetns(model);
-        cleanStyle(model);
-        if (model.type === 'Group' || model.type === 'Page') {
-            if (!model.contents) {
-                model.contents = [];
-            }
-            if (model.contents.length) {
-                for (var i = 0; i < model.contents.length; i++) {
-                    cleanInternal(model.contents[i]);
-                }
-            }
-        }
-        return model;
+	cleanEvetns(model);
+	cleanStyle(model);
+	if (model.type === 'Group' || model.type === 'Page') {
+	    if (!model.contents) {
+		model.contents = [];
+	    }
+	    if (model.contents.length) {
+		for (var i = 0; i < model.contents.length; i++) {
+		    cleanInternal(model.contents[i]);
+		}
+	    }
+	}
+	return model;
     }
 
     /**
      * Clean data model
+     * @name clean 
+     * @param {object} model 
+     * @param {type} force
      */
     function clean(model, force)
     {
-	model.type = 'Group';
-        if (model.version === 'wb1' && !force) {
-            return model;
-        }
-        var newModel = cleanInternal(model);
-        newModel.version = 'wb1';
-        return newModel;
+	if (!model.type) {
+	    model.type = 'Group';
+	}
+	if (model.version === 'wb1' && !force) {
+	    return model;
+	}
+	var newModel = cleanInternal(model);
+	newModel.version = 'wb1';
+	return newModel;
     }
 
     service.cleanMap = cleanMap;
