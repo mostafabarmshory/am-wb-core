2d294b364cd6ef8308d4f5d5b5851bc05ff299be
masood <masood@DESKTOP-GV3RBT4>
2018-10-22 17:38:32 +0330

    border test was rewritten for readability and reuse.

--- test/spec/controllers/wb-setting-border-test.js
+++ test/spec/controllers/wb-setting-border-test.js
@@ -37,114 +37,155 @@ describe('Setting border', function () {
 	$rootScope = _$rootScope_;
 	$injector = _$injector_;
     }));
-    
-    it('should not change original model', function () {
-		var page = $settings.getPage('border');
-		var scope = $rootScope.$new();
-		var ctrl = {};
-		
-		// TODO: check if the controller is a function
-		$injector.invoke(page.controller, ctrl, {
-		    '$scope': scope
-		});
-		
-		// Test #1: 
-		// scope.wbModel shouldn't have border property
-		scope.wbModel = {
-		    style:{}
-		};
-		scope.$digest();
-		expect(angular.isDefined(scope.wbModel.style.border)).toBe(false);
-		
-		/*
-		 * Test #2:
-		 * When the model is changed to the new one, old model shouldn't have changed.
-		 * Also, the new model(based on the new model) should have new properties.
-		 */
-		var wbModelNew = {
-		    style:{
-			border: {
-			    style: 'solid',
-			    color: '#26ea5e',
-			    width: '3px',
-			    radius: '20px'
-			}
-		    }
-		};
-		var wbModel = scope.wbModel;
-		scope.wbModel = wbModelNew;
-		scope.$digest();
-		
-		expect(angular.isDefined(wbModel.style.border)).toBe(false);
-		expect(angular.isDefined(scope.wbModel.style.border)).toBe(true);
-		expect(ctrl.style).toBe('solid');
-		expect(ctrl.color).toBe('#26ea5e');
-		
-		expect(ctrl.widthAll).toBe('3px');
-		expect(ctrl.width).toEqual({
-		    top: '3px',
-		    right: '3px',
-		    bottom: '3px',
-		    left: '3px'
-		});
-		expect(ctrl.radiusAll).toBe('20px');
-		expect(ctrl.radius).toEqual({
-		    topLeft: '20px',
-		    topRight: '20px',
-		    bottomLeft: '20px',
-		    bottomRight: '20px'
-		});
 
-		/*
-		 * Test #3:
-		 * When the model is changed to the new one, old model shouldn't have changed.
-		 * Also, the new model(based on the new model) should have new properties.
-		 */
-		var wbModelNew1 = {
-		    style:{
-			border: {
-			    style: 'solid',
-			    color: '#26ea5e',
-			    width: '3px 5px',
-			    radius: '10px 20px 30px'
-			}
-		    }
-		};
-		var wbModel1 = scope.wbModel;
-		scope.wbModel = wbModelNew1;
-		scope.$digest();
-		
-		expect(angular.isDefined(scope.wbModel.style.border)).toBe(true);
-		expect(ctrl.width).toEqual({
-		    top: '3px',
-		    bottom: '3px',
-		    right: '5px',
-		    left: '5px'
-		});
-		expect(ctrl.radius).toEqual({
-		    topLeft: '10px',
-		    topRight: '20px',
-		    bottomLeft: '20px',
-		    bottomRight: '30px'
-		});
-		
-		/*
-		 * Test #4:
-		 * top-width is changed.
-		 * The new wbModel should be same as before, except in top-width
-		 */
-		ctrl.width.top = '10px';
-		ctrl.widthChanged();
-		expect(scope.wbModel.style.border.width).toBe('10px 5px 3px 5px');
-		
-		/*
-		 * Test #5:
-		 * top-left radius is changed.
-		 * The new wbModel should be same as before, except in top-left
-		 */
-		ctrl.radius.topLeft = '50px';
-		ctrl.radiusChanged();
-		expect(scope.wbModel.style.border.radius).toBe('50px 20px 30px 20px');
-		
+
+    it('should not change original model', function () {
+
+	var page = $settings.getPage('border');
+	var scope = $rootScope.$new();
+	var ctrl = {};
+	$injector.invoke(page.controller, ctrl, {
+	    '$scope': scope
 	});
+	scope.wbModel = {
+	    style: {}
+	};
+	scope.$digest();
+	expect(angular.isDefined(scope.wbModel.style.border)).toBe(false);
+
+	scope.$destroy();
+	ctrl = null;
+	page = null;
+    });
+
+    it('should not change old model when the model is changed to the new one, and set correct values to the new model.', function () {
+
+	var page = $settings.getPage('border');
+	var scope = $rootScope.$new();
+	var ctrl = {};
+	$injector.invoke(page.controller, ctrl, {
+	    '$scope': scope
+	});
+	scope.wbModel = {
+	    style: {}
+	};
+	var wbModelNew = {
+	    style: {
+		border: {
+		    style: 'solid',
+		    color: '#26ea5e',
+		    width: '3px',
+		    radius: '20px'
+		}
+	    }
+	};
+	var wbModel = scope.wbModel;
+	scope.wbModel = wbModelNew;
+	scope.$digest();
+
+	expect(angular.isDefined(wbModel.style.border)).toBe(false);
+
+	expect(angular.isDefined(scope.wbModel.style.border)).toBe(true);
+	expect(ctrl.style).toBe('solid');
+	expect(ctrl.color).toBe('#26ea5e');
+	expect(ctrl.widthAll).toBe('3px');
+	expect(ctrl.width).toEqual({
+	    top: '3px',
+	    right: '3px',
+	    bottom: '3px',
+	    left: '3px'
+	});
+	expect(ctrl.radiusAll).toBe('20px');
+	expect(ctrl.radius).toEqual({
+	    topLeft: '20px',
+	    topRight: '20px',
+	    bottomLeft: '20px',
+	    bottomRight: '20px'
+	});
+
+	scope.$destroy();
+	ctrl = null;
+	wbModel = null;
+	wbModelNew = null;
+	page = null;
+    });
+
+    it('should set correct values to the new model if the items in width and radius are 1 || 2 || 3 || 4.', function () {
+
+	var page = $settings.getPage('border');
+	var scope = $rootScope.$new();
+	var ctrl = {};
+	$injector.invoke(page.controller, ctrl, {
+	    '$scope': scope
+	});
+
+	scope.wbModel = {
+	    style: {
+		border: {
+		    style: 'solid',
+		    color: '#26ea5e',
+		    width: '3px 5px',
+		    radius: '10px 20px 30px'
+		}
+	    }
+	};
+	scope.$digest();
+
+	expect(angular.isDefined(scope.wbModel.style.border)).toBe(true);
+	expect(ctrl.width).toEqual({
+	    top: '3px',
+	    bottom: '3px',
+	    right: '5px',
+	    left: '5px'
+	});
+	expect(ctrl.radius).toEqual({
+	    topLeft: '10px',
+	    topRight: '20px',
+	    bottomLeft: '20px',
+	    bottomRight: '30px'
+	});
+
+	scope.$destroy();
+	ctrl = null;
+	page = null;
+    });
+
+    it('should not change the main model except in the changed field (for width & radius)', function () {
+
+	var page = $settings.getPage('border');
+	
+	//width section
+	var scope = $rootScope.$new();
+	var ctrl = {};
+	$injector.invoke(page.controller, ctrl, {
+	    '$scope': scope
+	});
+	
+	scope.wbModel = {
+	    style: {
+		border: {
+		    style: 'solid',
+		    color: '#26ea5e',
+		    width: '5px 3px 3px 5px',
+		    radius: '10px 20px 30px'
+		}
+	    }
+	};
+	ctrl.width = {};
+	ctrl.radius = {};
+	
+	scope.$digest();
+	ctrl.width.top = '10px';
+	ctrl.widthChanged();
+	expect(scope.wbModel.style.border.width).toBe('10px 3px 3px 5px');
+
+	//radius section
+	ctrl.radius.topLeft = '50px';
+	ctrl.radiusChanged();
+	expect(scope.wbModel.style.border.radius).toBe('50px 20px 30px 20px');
+
+	scope.$destroy();
+	ctrl = null;
+	page = null;
+    });
 });
