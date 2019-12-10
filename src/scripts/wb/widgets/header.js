8f83d4a88588546be22073db1db2928aff5d53c4
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2019-12-10 06:53:19 +0330

    convert header to a group

--- src/scripts/wb/widgets/header.js
+++ src/scripts/wb/widgets/header.js
@@ -27,12 +27,12 @@ angular.module('am-wb-core')//
  * @name header
  * @description Manage a widget
  */
-.factory('WbWidgetHeader', function (WbWidgetAbstract) {
+.factory('WbWidgetHeader', function (WbWidgetGroup) {
     'use strict';
     function Widget($element, $parent){
-        WbWidgetAbstract.apply(this, [$element, $parent]);
+        WbWidgetGroup.apply(this, [$element, $parent]);
         this.addElementAttributes();
     }
-    Widget.prototype = Object.create(WbWidgetAbstract.prototype);
+    Widget.prototype = Object.create(WbWidgetGroup.prototype);
     return Widget;
 });
