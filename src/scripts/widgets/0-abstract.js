c2449bc87dbd5e427b643263e0f6458d5c914a47
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-01-26 19:34:12 +0330

    fixed: https://trello.com/c/vHsnfLHm

--- src/scripts/widgets/0-abstract.js
+++ src/scripts/widgets/0-abstract.js
@@ -126,7 +126,7 @@ angular.module('am-wb-core')//
          * 
          * @memberof WbAbstractWidget
          */
-        this.state = 'init';
+        this.state;
 
         this.actions = [];
         this.callbacks = [];
