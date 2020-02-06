35dae49e4ac61320eb3a5730e95ebfe118a62de5
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-02-06 08:45:03 +0330

    ??

--- dist/am-wb-core.js
+++ dist/am-wb-core.js
@@ -3800,6 +3800,7 @@ angular.module('am-wb-core').service('$widget', function(
 
 });
 
+
 /* 
  * The MIT License (MIT)
  * 
@@ -5418,6 +5419,12 @@ angular.module('am-wb-core').factory('WbWidgetElement', function(WbWidgetContain
 		var element = this.getElement();
 		return element.val.apply(element, arguments);
 	};
+	
+	// TODO: support:
+	// .attr()
+	// .prop()
+	// .removeAttr()
+	// .removeProp()
 	return Widget;
 });
 
